f5dbdb36319d849913f07e86841b12af4b8dd733
sumo6842 <sumo6842@gmail.com>
2021-08-23 15:06:32 +0700

    Filter event binding

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,4 +1,3 @@
-import { AuthorService } from './../author.service';
 // import { CourseService } from './courses.service';
 import { Component, OnInit } from '@angular/core';
 @Component({
@@ -6,27 +5,12 @@ import { Component, OnInit } from '@angular/core';
   // templateUrl: './course.component.html',
   // styleUrls: ['./course.component.css'
   template: `
-
-  <div (click)="onDivClicked()">
-    <button (click)="onSave($event)">Save</button>
-  </div>
+    <input #email (keyup.enter)="onKeyUp(email.value)"/>
   `
 })
 export class CourseComponent implements OnInit {
-  title = "List Of Course";
-  imageUrl = "https://images.unsplash.com/photo-1528127269322-539801943592?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80";
-
-  isActive = false;
-  colSpan = 2;
-
-  onSave($event:any) {
-    $event.stopPropagation();
-    console.log('Button was clicked', $event);
-  }
-
-  onDivClicked() {
-    console.log("Div was cliked!!!");
-    
+  onKeyUp(email:any) {
+     console.log(email);
   }
 
   ngOnInit(): void {
