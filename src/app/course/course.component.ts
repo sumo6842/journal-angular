0e8a2c41330fa7b34e7d02003e67809f29c51a41
sumo6842 <sumo6842@gmail.com>
2021-08-23 11:23:41 +0700

    binding class

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -6,23 +6,15 @@ import { Component, OnInit } from '@angular/core';
   // templateUrl: './course.component.html',
   // styleUrls: ['./course.component.css'
   template: `
-    <h2>{{title}}</h2>
-    <h2 [textContent] = "title"> </h2>s
-
-    <img src="{{imageUrl}}"/>
-    <img [src]="imageUrl"/>
-    <table>
-      <tr>
-        <td [attr.colspan]="colSpan">
-          
-        </td>
-      </tr>
-    </table>
+    <button class="btn btn-primary" [class.active]="isActive">Save</button> 
   `
 })
 export class CourseComponent implements OnInit {
   title = "List Of Course";
   imageUrl = "https://images.unsplash.com/photo-1528127269322-539801943592?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80";
+
+  isActive = true;
+
   colSpan = 2;
   ngOnInit(): void {
     // throw new Error('Method not implemented.');
