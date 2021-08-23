ccf433a1521227d6663fff133f1dfa0f7583a14a
sumo6842 <sumo6842@gmail.com>
2021-08-23 11:34:21 +0700

    binding css style object DOM

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -6,18 +6,15 @@ import { Component, OnInit } from '@angular/core';
   // templateUrl: './course.component.html',
   // styleUrls: ['./course.component.css'
   template: `
-    <table>
-      <tr>
-        <td [attr.colspan]="colSpan"></td>
-      </tr>
-    </table>
+    <button 
+    [style.backgroundColor]="isActive ? 'green' : 'red'">Save</button>
   `
 })
 export class CourseComponent implements OnInit {
   title = "List Of Course";
   imageUrl = "https://images.unsplash.com/photo-1528127269322-539801943592?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80";
 
-  isActive = true;
+  isActive = false;
 
   colSpan = 2;
   ngOnInit(): void {
