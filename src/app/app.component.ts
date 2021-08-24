7b79fb1071487fadd584f963670206687077b5c7
sumo6842 <sumo6842@gmail.com>
2021-08-24 17:12:29 +0700

    ngFor and change Detective

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  viewMode = '';
+  courses = [
+    {id: 1, name: 'Course 1'},
+    {id: 2, name: 'Course 2'},
+    {id: 3, name: 'Course 3'},
+  ]
+  onRemove(course:any) {
+    let index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }
 }
